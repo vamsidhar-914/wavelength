64ea84034138caf96ceea7f648ced3f631ec32f5
Kandula Vamshidhar Reddy <vamsidhar.kanudula@ibm.com>
2025-05-22 08:14:42 +0530

    build fix

--- src/env.js
+++ src/env.js
@@ -9,7 +9,7 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
-    DIRECT_URL: z.string().url().optional(),
+    DIRECT_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
